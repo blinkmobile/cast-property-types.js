535037cd45e99525b10902d9743a7a54e300b163
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-19 15:05:35 +1000

    extract implementation from legacy project

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,68 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({},{},[])
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.castPropertyValues = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var SKIP_TYPES = ['array', 'function', 'object'];
+
+var FALSE_VALUES = [ 0, '0', '', 'false', null, undefined ];
+
+var CAST = {
+  'boolean': function toBoolean (value) {
+    if (FALSE_VALUES.indexOf(value) !== -1) {
+      return false;
+    }
+    return true;
+  },
+  'number': function toNumber (value) {
+    var output;
+    if (typeof value === 'boolean') {
+      return value ? 1 : 0;
+    }
+    output = parseFloat(value);
+    // it's safe to use isNaN() on actual Number values
+    return isNaN(output) ? 0 : output;
+  },
+  'string': function toString (value) {
+    if (value && typeof value === 'object' && value.toString) {
+      return value.toString();
+    }
+    return '' + value;
+  }
+};
+
+/**
+* @param {Object} input key-value pairs to cast
+* @param {Object} example key-value pairs where values are of desired types
+* @returns {Object} a new Object with same properties as input but with cast values
+*/
+function castPropertyValues (input, example) {
+  var output;
+  if (!input || typeof input !== 'object') {
+    throw new TypeError('1st argument must be an Object');
+  }
+  if (!example || typeof example !== 'object') {
+    throw new TypeError('1st argument must be an Object');
+  }
+  output = {};
+  Object.keys(input).forEach(function (prop) {
+    var type;
+    if (!input.hasOwnProperty(prop)) {
+      return; // no property to cast
+    }
+    if (!example.hasOwnProperty(prop)) {
+      output[prop] = input[prop];
+      return; // no example from which to derive type
+    }
+    type = typeof example[prop];
+    if (SKIP_TYPES.indexOf(type) !== -1 || typeof input[prop] === type) {
+      output[prop] = input[prop];
+      return; // nothing further to do for these types
+    }
+    output[prop] = CAST[type](input[prop]);
+  });
+  return output;
+}
+
+module.exports = castPropertyValues;
+
+},{}]},{},[1])(1)
+});
---
+++ test/index.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// foreign modules
+
+var test = require('tape');
+
+// local modules
+
+var castPropertyValues = require('..');
+
+// this module
+
+require('tape-chai');
+
+test('castPropertyValues', function (t) {
+
+  t.test('is a function', function (tt) {
+    tt.isFunction(castPropertyValues);
+    tt.end();
+  });
+
+  t.test('throws an error for bad parameters', function (tt) {
+    tt['throws'](function () {
+      castPropertyValues(null, null);
+    }, Error);
+    tt['throws'](function () {
+      castPropertyValues({}, null);
+    }, Error);
+    tt['throws'](function () {
+      castPropertyValues(null, {});
+    }, Error);
+    tt.end();
+  });
+
+  t.end();
+});
+
+test('casting values', function (t) {
+  var fixture;
+  var example;
+  var values;
+  var result;
+
+  fixture = {
+    b: true,
+    b2b: true,
+    b2n: true,
+    b2s: true,
+    n: 123,
+    n2b: 123,
+    n2n: 123,
+    n2s: 123,
+    s: 'abc',
+    s2b: 'true',
+    s2n: '123',
+    s2s: 'abc',
+    o: {},
+    a: []
+  };
+  example = {
+    missing: true,
+    b2b: false,
+    b2n: 0,
+    b2s: '',
+    n2b: false,
+    n2n: 0,
+    n2s: '',
+    s2b: false,
+    s2n: 0,
+    s2s: '',
+    o: {},
+    a: []
+  };
+  values = {
+    b2n: 1,
+    b2s: 'true',
+    n2b: true,
+    n2s: '123',
+    s2b: true,
+    s2n: 123,
+    // expect these to be the originals
+    b: fixture.b,
+    b2b: fixture.b2b,
+    n: fixture.n,
+    n2n: fixture.n2n,
+    s: fixture.s,
+    s2s: fixture.s2s,
+    o: fixture.o,
+    a: fixture.a
+  };
+  result = castPropertyValues(fixture, example);
+
+  t.test('input and output share same number of properties', function (tt) {
+    tt.lengthOf(Object.keys(result), Object.keys(fixture).length);
+    tt.end();
+  });
+
+  t.test('input and output have same set of keys', function (tt) {
+    tt.sameMembers(Object.keys(fixture), Object.keys(result));
+    tt.end();
+  });
+
+  t.test('results have expected types', function (tt) {
+    var prop;
+    var value;
+    for (prop in values) {
+      if (values.hasOwnProperty(prop)) {
+        tt.property(result, prop, '[' + prop + ']');
+        value = values[prop];
+        switch (typeof value) {
+          case 'boolean':
+            tt.isBoolean(result[prop]);
+            break;
+          case 'number':
+            tt.isNumber(result[prop]);
+            break;
+          case 'string':
+            tt.isString(result[prop]);
+            break;
+          default:
+            tt.equal(
+              result[prop],
+              fixture[prop],
+              '[' + prop + '] unchanged due to non-primitive type');
+        }
+        if (!Array.isArray(value) || typeof value !== 'object') {
+          tt.equal(result[prop], value, '[' + prop + ']');
+        }
+      }
+    }
+    tt.end();
+  });
+
+  t.end();
+});
